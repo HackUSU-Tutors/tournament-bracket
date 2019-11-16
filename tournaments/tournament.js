83c3ac0985ca5c41b1c89f61bbd3ecc821055c35
Nathaniel Sneddon <nathaniel.sneddon@gmail.com>
2019-11-16 12:34:29 -0700

    Sucess!

--- tournaments/tournament.js
+++ tournaments/tournament.js
@@ -1,8 +1,10 @@
 const express = require('express');
-var router = express.Router();
+const router = express.Router();
+const auth = require('../middleware/auth')
 
 // const Match = require('./models/Match');
 const Tournament = require('../models/Tournament')
+const User = require('../models/User')
 
 
 // @route /tournaments/:tour_id
@@ -17,6 +19,37 @@ router.get('/:tour_id', async (req, res) => {
     catch(error){
         console.error(error.message);
         res.status(500).send("Internal Server Error");
+    }
+});
+
+// @route /tournaments/create
+router.post('/create', auth, async (req, res) => {
+    try {
+        const {
+            name,
+            matches
+        } = req.body;
+        console.log(name);
+
+        const user = await User.findOne({ _id: req.user.id});
+        if(!user){
+            return res.status(400).json({ error: "No person found with token"});
+        }
+
+        var tournament = new Tournament({
+            name,
+            matches
+        });
+        // console.log("huihio");
+        tournament.save();
+        // console.log("huhuhuhuhuhuuhuh");
+        // console.log(user);
+        user.tournaments.push(tournament._id);
+        user.save();        
+        return res.status(200).send("Success!");
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).send("internal server error");
     }
 });
 
