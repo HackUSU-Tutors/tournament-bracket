43e77b56d117ed7a713b9e507d050eaa25fc510e
Sterling Tolley <sterlingj.tolley@gmail.com>
2019-11-15 22:02:25 -0700

    Serving static files from public

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 var port = 8080;
 
 app.get('/', function (req, res) {
-	res.send('hello world');
+	res.render('index', {name: 'Tournament'});
 });
 
 app.listen(port, () => {console.log(`App Runing on Port ${port}`);});
